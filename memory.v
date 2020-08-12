14db76318273aa667c5a93226ead2f5548d3fda7
Alireza Zarenejad <alireza.zarenjead99@gmail.com>
2020-08-12 20:51:42 +0430

    change memory

--- memory.v
+++ memory.v
@@ -2,50 +2,26 @@
 module memory(input clk, rst, mem_read, mem_write,input[31:0] address, write_data,
             output wire[31:0] read_data, output ready);
 	
-	reg[7:0] data[0:511];
-	wire [31:0] address4k = {address[31:2], 2'b0} - 32'd1024;
-	
-	wire [31:0] address4k_p1 = {address4k[31:2], 2'b01}; // address4k + 1
-	wire [31:0] address4k_p2 = {address4k[31:2], 2'b10}; // address4k + 2
-	wire [31:0] address4k_p3 = {address4k[31:2], 2'b11}; // address4k + 3
+	reg[31:0] data[0:63];
+	wire[31:0] diff = address - 32'd1024;
+	wire [31:0] address4k = {2'b0, diff[31:2]};
 
 	assign ready = 1'b1;
-	assign read_data = (mem_read == 1'b1) ? 
-			{data[address4k], data[address4k_p1], data[address4k_p2], data[address4k_p3]}
-			: 32'b0;
+
+	assign read_data = (mem_read == 1'b1) ? data[address4k] : 32'b0;
 
 	always @(posedge clk, posedge rst) begin
 		if (rst) begin
-			data[0] <= 8'b00010000;
-			data[1] <= 8'b00000000;
-			data[2] <= 8'b00000000;
-			data[3] <= 8'b00000000;
-			
-			data[4] <= 8'b11000000;
-			data[5] <= 8'b00100010;
-			data[6] <= 8'b00000000;
-			data[7] <= 8'b00000000;
-			
-			data[8] <= 8'b00000000;
-			data[9] <= 8'b01100100;
-			data[10] <= 8'b00000000;
-			data[11] <= 8'b00000000;
-			
-			data[12] <= 8'b00000000;
-			data[13] <= 8'b10100110;
-			data[14] <= 8'b00000000;
-			data[15] <= 8'b00000000;
+
+			data[0] = 32'b00010000_00000000_00000000_00000000;
+			data[1] = 32'b11000000_00100010_00000000_00000000;
+			data[2] = 32'b00000000_01100100_00000000_00000000;
+			data[3] = 32'b00000000_10100110_00000000_00000000;
+
 		end
 		else if (mem_write) begin
-			data[address4k_p3] = write_data[7:0];
-			// write to data[address + 1]
-			data[address4k_p2] = write_data[15:8];
-			
-			// write to data[address + 2]
-			data[address4k_p1] = write_data[23:16];
+			data[address4k] <= write_data;
 
-			// write to data[address + 3]
-			data[address4k] = write_data[31:24];
 		end
 	end
 
