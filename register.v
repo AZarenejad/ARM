d1a5dac56583dfab0bb7045cbffe89e44b32881a
Alireza Zarenejad <alireza.zarenjead99@gmail.com>
2020-08-15 11:34:07 +0430

    latest

--- register.v
+++ register.v
@@ -24,3 +24,20 @@ module status_register(input clk, rst, ld, input [3:0] data_in, output reg[3:0] 
 	end
 	
 endmodule
+
+
+module flush_register(clk, rst, flush, ld, in, out);
+
+	parameter WORD_LENGTH = 32;
+	input clk, rst, flush, ld;
+	input[WORD_LENGTH-1:0] in;
+	output reg[WORD_LENGTH-1:0] out;
+
+	always@(posedge clk, posedge rst) 
+	begin
+		if (rst) out <= 0;
+		else if (ld && flush) out <= 0;
+		else if (ld) out <= in;
+	end
+	
+endmodule
